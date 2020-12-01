a840228f46f70b3d74935333b73415a90bba8064
nathan <nate01776@gmail.com>
2020-12-01 10:59:44 -0500

    solution for day 1

---
+++ 01/1/build/index.rb
@@ -0,0 +1,19 @@
+# sudo docker build -t advent/1 .
+# sudo docker run advent/1:latest
+
+path = "./seed.txt"
+text = File.read(path)
+
+textArray = text.split(/\n/)
+
+targetValue = 2020
+
+textArray.each do |i|
+    currentNum = i;
+
+    textArray.each do |n|
+        if n.to_i + i.to_i == targetValue
+            puts n.to_i * i.to_i;
+        end
+    end
+end
---
+++ 01/2/build/index.rb
@@ -0,0 +1,24 @@
+# sudo docker build -t advent/2 .
+# sudo docker run advent/2:latest
+
+path = "./seed.txt"
+text = File.read(path)
+
+textArray = text.split(/\n/)
+
+targetValue = 2020
+
+textArray.each do |i|
+    currentNum = i;
+
+    textArray.each do |n|
+        textArray.each do |x|
+            if n.to_i + i.to_i + x.to_i == targetValue
+                puts n;
+                puts i;
+                puts x;
+                puts n.to_i * i.to_i * x.to_i;
+            end
+        end
+    end
+end
